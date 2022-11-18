41fef0695a1f8130e1eb6f62f087907b9c517f24
MDV2II <surmachb.marduk@gmail.com>
2022-11-18 16:01:26 +0200

    init commit

---
+++ src/main/java/com/example/demoEmployess/DemoEmployessApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoEmployess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmployessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEmployessApplication.class, args);
+	}
+
+}
